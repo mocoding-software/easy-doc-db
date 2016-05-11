6eee919186596e3563502005306e57819ef226ef
Dmytro Khomyshyn <dima.khomyshyn@mocoding.com>
2016-05-11 14:58:38 +0300

    #1899 Added xml serialazer

---
+++ src/Mocoding.EasyDocDb.Xml/XmlSerializer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mocoding.EasyDocDb.Xml
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class XmlSerializer : IDocumentSerializer
+    {
+        public string Type => "xml";
+
+        public string Serialize<T>(T data) where T : class, new()
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+            using (var writer = new StringWriter())
+            {
+                serializer.Serialize(writer, data);
+                return writer.ToString();
+            }
+        }
+
+        public T Deserialize<T>(string content) where T : class, new()
+        {
+            var serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+            using (var reader = new StringReader(content))
+            {
+                return (T)serializer.Deserialize(reader);
+            }
+        }
+    }
+}
