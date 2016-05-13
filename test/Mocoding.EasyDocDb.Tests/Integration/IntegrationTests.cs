abc61045f31d61248469ff335b0659c076710226
moLirik <kirill.mehtiev@mocoding.com>
2016-05-13 14:38:57 +0300

    #1890 Added several integration tests

--- test/Mocoding.EasyDocDb.Tests/Integration/IntegrationTests.cs
+++ test/Mocoding.EasyDocDb.Tests/Integration/IntegrationTests.cs
@@ -52,7 +52,8 @@ namespace Mocoding.EasyDocDb.Tests.IntegrationTests
 
             // exists in docCollection
             var actualFromColl = docCollection.All()
-                .Where(_ => _.Data.FullName == _person.FullName && _.Data.Salary == _person.Salary).FirstOrDefault();
+                .Where(_ => _.Data.FullName == expectedDoc.Data.FullName && _.Data.Salary == expectedDoc.Data.Salary)
+                .FirstOrDefault();
 
             // exists on disk
             DirectoryInfo dirInfo = new DirectoryInfo(@ref);
@@ -66,7 +67,75 @@ namespace Mocoding.EasyDocDb.Tests.IntegrationTests
 
             Assert.Equal(expectedDoc.Data.Salary, actualFromDisk.Salary);
             Assert.Equal(expectedDoc.Data.FullName, actualFromDisk.FullName);
+        }
 
+        [Fact]
+        public async Task DeleteDocumentTest()
+        {
+            // Arrange
+            var storage = new EmbeddedStorage();
+            var serializer = new JsonSerializer();
+            var @ref = Path.Combine(REF, "deleteTest");
+
+            // Act
+            var docCollection = new DocumentsCollection<Person>(@ref, storage, serializer);
+            await docCollection.Init();
+
+            var expectedDoc = docCollection.New();
+            expectedDoc.Data.Salary = _person.Salary;
+            expectedDoc.Data.FullName = _person.FullName;
+            await expectedDoc.Save();
+
+            await expectedDoc.Delete();
+
+            var actualFromColl = docCollection.All()
+                .Where(_ => _.Data.FullName == expectedDoc.Data.FullName && _.Data.Salary == expectedDoc.Data.Salary)
+                .FirstOrDefault();
+
+            DirectoryInfo dirInfo = new DirectoryInfo(@ref);
+            var actualFile = dirInfo.GetFiles("*.json").OrderBy(_ => _.CreationTime).LastOrDefault();
+
+            // Assert
+            Assert.Null(actualFromColl);
+            Assert.Null(actualFile);
+        }
+
+        [Fact]
+        public async Task UpdateDocumentTest()
+        {
+            // Arrange
+            var storage = new EmbeddedStorage();
+            var serializer = new JsonSerializer();
+            var @ref = Path.Combine(REF, "updateTest");
+            var newName = Guid.NewGuid().ToString();
+
+            // Act
+            var docCollection = new DocumentsCollection<Person>(@ref, storage, serializer);
+            await docCollection.Init();
+
+            var expectedDoc = docCollection.New();
+            expectedDoc.Data.Salary = _person.Salary;
+            expectedDoc.Data.FullName = _person.FullName;
+            await expectedDoc.Save();
+            await expectedDoc.SyncUpdate(_ => _.FullName = newName);
+
+            // exists in docCollection
+            var actualFromColl = docCollection.All()
+                .Where(_ => _.Data.FullName == expectedDoc.Data.FullName && _.Data.Salary == expectedDoc.Data.Salary)
+                .FirstOrDefault();
+
+            // exists on disk
+            DirectoryInfo dirInfo = new DirectoryInfo(@ref);
+            var actualFile = dirInfo.GetFiles("*.json").OrderBy(_ => _.CreationTime).LastOrDefault();
+            var actualPerson = File.ReadAllText(actualFile.FullName);
+            var actualFromDisk = serializer.Deserialize<Person>(actualPerson);
+
+            // Assert
+            Assert.Equal(expectedDoc.Data.Salary, actualFromColl.Data.Salary);
+            Assert.Equal(expectedDoc.Data.FullName, actualFromColl.Data.FullName);
+
+            Assert.Equal(expectedDoc.Data.Salary, actualFromDisk.Salary);
+            Assert.Equal(expectedDoc.Data.FullName, actualFromDisk.FullName);
         }
 
     }
