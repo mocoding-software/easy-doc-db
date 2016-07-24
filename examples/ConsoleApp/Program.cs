46b07071b78987ac4141c8cfe13a1d8f52f5e632
Denys Miasoutov <denys.miasoutov@mocoding.com>
2016-07-23 21:31:04 -0400

    Updated comments and packOptions.

--- examples/ConsoleApp/Program.cs
+++ examples/ConsoleApp/Program.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using Mocoding.EasyDocDb;
-using Mocoding.EasyDocDb.Json;
+using Mocoding.EasyDocDb.ConsoleCrudSample;
 
-namespace Mocoding.EasyDocDb.ConsoleCrudSample
+namespace Mocoding.EasyDocDb.ConsoleApp
 {
     public class Program
     {        
--- src/Mocoding.EasyDocDb/Interfaces.cs
+++ src/Mocoding.EasyDocDb/Interfaces.cs
@@ -6,13 +6,34 @@ using System.Threading.Tasks;
 
 namespace Mocoding.EasyDocDb
 {
+    /// <summary>
+    /// Allows 
+    /// </summary>
     public interface IRepository
     {
+        /// <summary>
+        /// Initializes the document that is stored in a single file.
+        /// It will read the file if it is already exists.
+        /// </summary>
+        /// <typeparam name="T">Type of the document.</typeparam>
+        /// <param name="conn">Path to file or other reference to where the file is stored on.</param>
+        /// <returns></returns>
         Task<IDocument<T>> Init<T>(string conn) where T : class, new();
 
+        /// <summary>
+        /// Initializes the collection of documents that is stored as a set of files.
+        /// It will read all existing files and populate documents from them.
+        /// </summary>
+        /// <typeparam name="T">Type of the document.</typeparam>
+        /// <param name="conn">Path to folder where documents are stored</param>
+        /// <returns></returns>
         Task<IDocumentCollection<T>> InitCollection<T>(string conn) where T : class, new();
     }
 
+    /// <summary>
+    /// Represents a collection of documents and allows certain operations to be performed.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IDocumentCollection<T> where T : class, new()
     {
         /// <summary>
@@ -31,23 +52,67 @@ namespace Mocoding.EasyDocDb
         IDocument<T> New();
     }
 
+    /// <summary>
+    /// Represents a document and allows certain operations to be performed.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IDocument<out T> where T : new()
     {
+        /// <summary>
+        /// Returns the document content.
+        /// </summary>
+        /// <value>
+        /// The data.
+        /// </value>
         T Data { get; }
 
+        /// <summary>
+        /// Allows to update document data in a thread-safe manner.
+        /// </summary>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
         Task SyncUpdate(Action<T> data);
 
+        /// <summary>
+        /// Allows saving the document data in a thread-safe manner.
+        /// </summary>
+        /// <returns></returns>
         Task Save();
 
+        /// <summary>
+        /// Allows deleting the document data in a thread-safe manner.
+        /// </summary>
+        /// <returns></returns>
         Task Delete();
     }
 
+    /// <summary>
+    /// A facade for serializer used to serialize and deserialize content of the document.
+    /// </summary>
     public interface IDocumentSerializer
     {
+        /// <summary>
+        /// Identifies the serializer by returning an extension to be used to save documents as files.
+        /// </summary>
+        /// <value>
+        /// The type.
+        /// </value>
         string Type { get; }
 
+        /// <summary>
+        /// Serializes the specified data.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data">The data.</param>
+        /// <returns></returns>
         string Serialize<T>(T data) where T : class, new();
 
+        /// <summary>
+        /// Deserializes the specified content.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="content">The content.</param>
+        /// <returns></returns>
         T Deserialize<T>(string content) where T : class, new();
     }
 
