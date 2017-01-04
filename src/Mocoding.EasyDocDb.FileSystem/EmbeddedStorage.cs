788206bb58978f5cb25b82f23b26f9f57f83db5b
offbeatful <dennis.miasoutov@mocoding.com>
2017-01-04 13:35:34 -0500

    Updated versions. Added missed files of new lib.

---
+++ src/Mocoding.EasyDocDb.FileSystem/EmbeddedStorage.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("Mocoding.EasyDocDb.Tests")]
+namespace Mocoding.EasyDocDb.FileSystem
+{
+    internal class EmbeddedStorage : IDocumentStorage
+    {
+        public async Task<string> Read(string @ref)
+        {
+            try
+            {
+                if (!File.Exists(@ref))
+                    return string.Empty;
+
+                var content = await Task.Run(() => File.ReadAllText(@ref));
+                return content;
+            }
+            catch (Exception ex)
+            {
+                throw new StorageException($"Failed to read content from file {@ref}", ex);
+            }
+        }
+
+        public async Task Write(string @ref, string document)
+        {
+            try
+            {
+                await Task.Run(() => File.WriteAllText(@ref, document));
+            }
+            catch (Exception ex)
+            {
+                throw new StorageException($"Failed to write content to file {@ref}", ex);
+            }
+        }
+
+        public async Task Delete(string @ref)
+        {
+            try
+            {
+                if (File.Exists(@ref))
+                    await Task.Run(() => File.Delete(@ref));
+            }
+            catch (Exception ex)
+            {
+                throw new StorageException($"Failed to delete file {@ref}", ex);
+            }
+        }
+
+        public async Task<string[]> Enumerate(string collectionRef)
+        {
+            try
+            {
+                if (Directory.Exists(collectionRef))
+                    return await Task.Run(() => Directory.EnumerateFiles(collectionRef).ToArray());
+            }
+            catch (Exception ex)
+            {
+                throw new StorageException($"Failed to enumerate files in directory {collectionRef}", ex);
+            }
+
+            try
+            {
+                Directory.CreateDirectory(collectionRef);
+            }
+            catch (Exception ex)
+            {
+                throw new StorageException($"Failed to create directory {collectionRef}", ex);
+            }
+
+            return new string[0];
+        }
+
+        public string NewRef(string collectionRef, string name)
+        {
+            return Path.Combine(collectionRef, name);
+        }
+    }
+}
