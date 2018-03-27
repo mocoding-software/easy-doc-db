81185c9908b4402e3bcb8a8e1894c963f18ce89f
Dennis Miasoutov <dennis.miasoutov@mocoding.com>
2018-03-26 20:37:54 -0400

    Updated target framework, dependencies and descriptions.

--- src/Mocoding.EasyDocDb.AzureStorage/AzureStorage.cs
+++ src/Mocoding.EasyDocDb.AzureStorage/AzureStorage.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Mocoding.AspNetCore.ODataApi.EasyDocDb.AzureStorage;
 
 namespace Mocoding.EasyDocDb.AzureStorage
 {
